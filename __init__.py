84539a816268fe6e7fdeeb8a6ecbf920be02e93b
caballeroantonio <caballeroantonio@hotmail.com>
2025-05-05 21:15:18 -0600

    [FEAT] - Imagenes de reemplazo

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of Elixir (caballeroantonio@hotmail.com). See LICENSE file for full copyright and licensing details.
+
+#from . import models
---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Elixir (caballeroantonio@hotmail.com). See LICENSE file for full copyright and licensing details.
+
+{
+	'name':						'Cafe Express',
+	'version':					'18.0',
+	'sequence':					100,
+	'summary':					'Cafe',
+	'description':				'''
+False
+	''',
+	'icon':						'/website/static/description/icon.png',
+	'author':					'Marco Antonio García <caballeroantonio@hotmail.com>',
+	'contributors':				'Antonio García',
+	'maintainer':				'caballeroantonio@hotmail.com',
+	'website':					'http://caballeroantonio.ddns.net',
+	'url':						'https://github.com/caballeroantonio/elixir_cafesv',
+	'license':					'Other proprietary',
+	'price':					100,
+	'currency':					'USD',
+	'category':					'Sitio web',
+	'depends': [
+								'website',
+	],
+	'external_dependencies':	{'python': [], 'bin': []},
+	'data': [
+								'data/website_theme.xml',
+	],
+	'demo': [
+	],
+	'application':				False,
+	'installable':				True,
+	'auto_install':				False,
+	
+}
